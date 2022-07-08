c25e3e7a44f593eea067298152af6e4088d30853
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-07-08 14:58:24 +0200

    adds fullscreen option

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const skipButton = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 const progressBar = player.querySelector('.progress__filled');
 const progress = player.querySelector('.progress');
+const fullscreen = player.querySelector('.fullscreen');
 
 // functions
 function playVideo() {
@@ -31,8 +32,12 @@ function handleProgress() {
 
 function updateSlider(e) {
   const percent = e.offsetX * 100 / progress.clientWidth;
-  progressBar.style.flexBasis = `${percent}%`; 
+  progressBar.style.flexBasis = `${percent}%`;
   video.currentTime = percent * video.duration / 100;
+}
+
+function toggleFullscreen() {
+  video.requestFullscreen();
 }
 
 // hook up elements to functions
@@ -55,3 +60,4 @@ progress.addEventListener('click', updateSlider);
 progress.addEventListener('mousemove', (e) => mousedown && updateSlider(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+fullscreen.addEventListener('click', toggleFullscreen);
