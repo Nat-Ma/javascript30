b9677f68fbcf5c530c85131ec2784e9f0fd50e33
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-07-08 13:43:01 +0200

    implemented skip button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,18 +2,31 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
+const skipButton = player.querySelectorAll('[data-skip]');
 
 // functions
 function playVideo() {
   video.paused ? video.play() : video.pause();
+  console.dir(video);
 }
 
 function toggleIcon() {
-  video.paused ? toggle.innerHTML = '▐▐' : toggle.innerHTML = '►'
+  video.paused ? toggle.innerHTML = '►' : toggle.innerHTML = '▐▐'
 }
+
+function skipTime() {
+  video.currentTime += Number(this.dataset.skip);
+};
 
 // hook up elements to functions
 video.addEventListener('click', playVideo);
-toggle.addEventListener('click', playVideo);
 video.addEventListener('play', toggleIcon);
 video.addEventListener('pause', toggleIcon);
+
+toggle.addEventListener('click', playVideo);
+
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode === 32) playVideo();
+});
+
+skipButton.forEach(b => b.addEventListener('click', skipTime));
